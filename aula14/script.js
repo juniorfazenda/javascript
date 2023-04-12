a73ef7e259ac07e9f48c244b0b1baf59b04848b3
juniorfazenda <juniorfazenda@gmail.com>
2023-04-12 13:30:32 -0300

    finalização exercício 14

--- aula14/script.js
+++ aula14/script.js
@@ -1,1 +1,21 @@
-alert('olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('foto')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 11
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.innerHTML = '<img src="img/fotomanha.png">'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.innerHTML = '<img src="img/fototarde.png">'
+        document.body.style.background = '#b9846f'
+    } else {
+        //BOA NOITE!
+        img.innerHTML = '<img src="img/fotonoite.png">'
+        document.body.style.background = '#515154'
+    }
+}
