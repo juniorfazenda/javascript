82c45088edf0bdbe94e4b16cd2407ad76146609e
juniorfazenda <juniorfazenda@gmail.com>
2023-04-24 18:08:42 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)*/
+
+console.log('Vai começar...')
+for (var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
